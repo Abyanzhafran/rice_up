695e56f4557ff6cd58320424064c4bd01f7803fb
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-07-31 08:48:40 +0700

    feat: add dashboard page

---
+++ src/layouts/dashboardLayout.vue
@@ -0,0 +1,110 @@
+<template>
+    <q-layout view="hHh Lpr lff">
+      <q-header elevated class="bg-black">
+        <q-toolbar>
+          <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+          <q-toolbar-title>Header</q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+
+      <q-drawer
+        v-model="drawer"
+        show-if-above
+        :width="200"
+        :breakpoint="500"
+        bordered
+        class="bg-grey-3"
+      >
+        <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+          <q-list>
+
+            <template v-for="(menuItem, index) in menuList" :key="index">
+              <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+                <q-item-section avatar>
+                  <q-icon :name="menuItem.icon" />
+                </q-item-section>
+                <q-item-section>
+                  {{ menuItem.label }}
+                </q-item-section>
+              </q-item>
+              <q-separator :key="'sep' + index"  v-if="menuItem.separator" />
+            </template>
+
+          </q-list>
+        </q-scroll-area>
+
+         <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            <div class="text-weight-bold">Razvan Stoenescu</div>
+            <div>@rstoenescu</div>
+          </div>
+        </q-img>
+      </q-drawer>
+
+      <q-page-container>
+        <q-page padding>
+          <p v-for="n in 15" :key="n">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Fugit nihil praesentium molestias a adipisci, dolore vitae odit,
+            quidem consequatur optio voluptates asperiores pariatur eos numquam
+            rerum delectus commodi perferendis voluptate?
+          </p>
+        </q-page>
+      </q-page-container>
+    </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+const menuList = [
+  {
+    icon: 'inbox',
+    label: 'Inbox',
+    separator: true,
+  },
+  {
+    icon: 'send',
+    label: 'Outbox',
+    separator: false,
+  },
+  {
+    icon: 'delete',
+    label: 'Trash',
+    separator: false,
+  },
+  {
+    icon: 'error',
+    label: 'Spam',
+    separator: true,
+  },
+  {
+    icon: 'settings',
+    label: 'Settings',
+    separator: false,
+  },
+  {
+    icon: 'feedback',
+    label: 'Send Feedback',
+    separator: false,
+  },
+  {
+    icon: 'help',
+    iconColor: 'primary',
+    label: 'Help',
+    separator: false,
+  },
+];
+
+export default {
+  setup() {
+    return {
+      drawer: ref(false),
+      menuList,
+    };
+  },
+};
+</script>
