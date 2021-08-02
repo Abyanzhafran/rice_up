1dcd13e04906a352f15c32d9f8f73595d25a326c
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-08-02 08:33:17 +0700

    feat(ui): :lipstick: add margin mobile #card stuck

--- src/pages/dashboard/Account.vue
+++ src/pages/dashboard/Account.vue
@@ -1,12 +1,38 @@
 <template>
-  <q-page class="flex justify-center">
-    <div class="pt-10 flex flex-col items-center gap-4">
+  <q-page class="flex justify-center mx-6">
+    <div class="w-full mx-auto pt-10 flex flex-col items-center gap-4">
       <q-avatar class="w-20 h-20">
         <img src="https://cdn.quasar.dev/img/avatar.png" />
       </q-avatar>
 
       <span class="font-semibold text-lg pt-2">Jack Hoodward</span>
       <q-btn class="capitalize w-56" outline color="black">Kelas Saya</q-btn>
+      <!-- <q-card class="mx-auto rounded-2xl" flat bordered>
+        <q-card-section class="p-2 flex items-center gap-4" horizontal>
+          <div>
+            <q-img
+              class="rounded-2xl w-28"
+              src="https://cdn.quasar.dev/img/parallax2.jpg"
+            />
+          </div>
+          <div class="w-auto">
+            <div class="text-xl line-clamp-2">Drone sebagai pengawas lahan</div>
+            <div class="font-normal">Menpertani</div>
+          </div>
+          <div class="w-28">
+            <q-circular-progress
+              show-value
+              size="86px"
+              :value="value"
+              :thickness="0.3"
+              color="teal"
+              track-color="grey-3"
+            >
+              <span class="m-4 text-xl font-semibold">{{ value }}%</span>
+            </q-circular-progress>
+          </div>
+        </q-card-section>
+      </q-card> -->
     </div>
   </q-page>
 </template>
@@ -16,9 +42,11 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "PageAccount",
-  // setup() {
-
-  // },
+  setup() {
+    return {
+      value: 81,
+    };
+  },
 });
 </script>
 
