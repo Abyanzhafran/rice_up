af77be291f80ea693e8fee4b7c2bd3e840ca3da3
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-08-03 22:35:29 +0700

    feat(ui): :lipstick: add card carousel at home dashboardMobile

--- src/pages/dashboard/Home.vue
+++ src/pages/dashboard/Home.vue
@@ -1,17 +1,113 @@
 <template>
   <q-page class="flex justify-center mx-6">
     <div class="w-full mx-auto pt-10 flex flex-col items-center gap-4">
+      <q-carousel
+        v-model="slide"
+        transition-prev="jump-right"
+        transition-next="jump-left"
+        swipeable
+        animated
+        control-color="dark"
+        navigation-icon="radio_button_unchecked"
+        navigation
+        padding
+        height="175px"
+        class="bg-transparent text-dark"
+      >
+        <q-carousel-slide
+          name="style"
+          class="column no-wrap flex-center"
+        >
+          <q-card
+            class="rounded-2xl"
+            flat
+            bordered
+          >
+            <q-card-section horizontal>
+              <q-card-section>
+                {{ lorem }}
+              </q-card-section>
+
+              <q-img
+                class="col-5"
+                src="https://cdn.quasar.dev/img/parallax2.jpg"
+              />
+            </q-card-section>
+          </q-card>
+        </q-carousel-slide>
+        <q-carousel-slide
+          name="tv"
+          class="column no-wrap flex-center"
+        >
+          <q-card
+            class="rounded-2xl"
+            flat
+            bordered
+          >
+            <q-card-section horizontal>
+              <q-card-section>
+                {{ lorem }}
+              </q-card-section>
+
+              <q-img
+                class="col-5"
+                src="https://cdn.quasar.dev/img/parallax2.jpg"
+              />
+            </q-card-section>
+          </q-card>
+        </q-carousel-slide>
+        <q-carousel-slide
+          name="layers"
+          class="column no-wrap flex-center"
+        >
+          <q-card
+            class="rounded-2xl"
+            flat
+            bordered
+          >
+            <q-card-section horizontal>
+              <q-card-section>
+                {{ lorem }}
+              </q-card-section>
+
+              <q-img
+                class="col-5"
+                src="https://cdn.quasar.dev/img/parallax2.jpg"
+              />
+            </q-card-section>
+          </q-card>
+        </q-carousel-slide>
+      </q-carousel>
       <span class="font-bold text-2xl">Kelas Populer</span>
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+
+const slides = [
+  {
+    name: 'tv',
+  },
+  {
+    name: 'hp',
+  },
+  {
+    name: 'laptop',
+  },
+  {
+    name: 'mount',
+  },
+];
 
 export default defineComponent({
-  // setup() {
-
-  // },
+  setup() {
+    return {
+      slide: ref('style'),
+      lorem: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+      slides,
+    };
+  },
 });
 </script>
