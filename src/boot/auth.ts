37c987680281151d57b5b653b0f331a2eb638c7d
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-08-10 22:34:09 +0700

    feat: :sparkles: improve auth guard logic
    - add auth guard logic to router.beforeResolve()

--- src/boot/auth.ts
+++ src/boot/auth.ts
@@ -3,7 +3,7 @@ import { Loading } from 'quasar';
 import { boot } from 'quasar/wrappers';
 import { createGlobalState } from '@vueuse/core';
 import { auth } from 'src/firebase';
-import type { RouteLocationRaw } from 'vue-router';
+import type { RouteLocationRaw, RouteLocationNormalizedLoaded } from 'vue-router';
 
 type authStateHandler = () => void;
 
@@ -18,14 +18,18 @@ export const authStateUtils = () => {
   const authState = useAuthState();
 
   return {
-    subscribeAfterAuthOnce: (handler: authStateHandler) => {
+    subscribeOnceAfterAuth: (handler: authStateHandler) => {
       authState.afterAuthHandlerOnce.push(handler);
     },
-    subscribeAfterUnauthOnce: (handler: authStateHandler) => {
+    subscribeOnceAfterUnauth: (handler: authStateHandler) => {
       authState.afterUnauthHandlerOnce.push(handler);
     },
   };
 };
+
+const getRouteGuardType = (route: RouteLocationNormalizedLoaded) => route.meta.guard || 'default';
+const authFailRedirect: RouteLocationRaw = '/welcome';
+const noAuthFailRedirect: RouteLocationRaw = '/splash';
 
 export default boot(({
   app, router, urlPath, /* redirect, */
@@ -35,7 +39,6 @@ export default boot(({
   const getCurrentRoute = (isFirstLoad = false) => (isFirstLoad
     ? router.resolve(urlPath)
     : router.currentRoute.value);
-  const getRouteGuardType = (isFirstLoad = false) => getCurrentRoute(isFirstLoad).meta.guard || 'default';
 
   const onAfterAuthenticated = () => {
     authState.afterAuthHandlerOnce.forEach((handler) => handler());
@@ -45,18 +48,6 @@ export default boot(({
   const onAfterUnauthenticated = () => {
     authState.afterUnauthHandlerOnce.forEach((handler) => handler());
     authState.afterUnauthHandlerOnce = [];
-  };
-
-  const getRedirectUrl = (isAuthenticated = false, isFirstLoad = false): RouteLocationRaw => {
-    const currentRouteGuard = getRouteGuardType(isFirstLoad);
-    if (currentRouteGuard === 'auth' && !isAuthenticated) {
-      return '/welcome';
-    }
-    if (currentRouteGuard === 'no-auth' && isAuthenticated) {
-      return '/splash';
-    }
-
-    return urlPath;
   };
 
   auth
@@ -71,17 +62,26 @@ export default boot(({
       }
     });
 
+  // watch auth status, then redirect
   watch(() => [authState.user, authState.isAwaiting] as const, ([user], old) => {
     const isFirstLoad = old === undefined;
 
-    return !isFirstLoad
-      && router.push(getRedirectUrl(!!user, isFirstLoad));
+    if (!isFirstLoad) {
+      const currentRouteGuard = getRouteGuardType(getCurrentRoute(isFirstLoad));
+
+      if (currentRouteGuard === 'auth' && !user) {
+        void router.push(authFailRedirect);
+      }
+      if (currentRouteGuard === 'no-auth' && user) {
+        void router.push(noAuthFailRedirect);
+      }
+    }
   }, { immediate: true });
 
   // show loading ui when route has auth guard type && waiting the auth status
   watch(() => authState.isAwaiting, (v, old) => {
     const isFirstLoad = old === undefined;
-    const routeGuardType = getRouteGuardType(isFirstLoad);
+    const routeGuardType = getRouteGuardType(getCurrentRoute(isFirstLoad));
 
     if (routeGuardType === 'auth' && v) {
       Loading.show({ message: 'Sedang mengautentikasi...' });
@@ -89,4 +89,16 @@ export default boot(({
       Loading.hide();
     }
   }, { immediate: true });
+
+  router.beforeResolve((to, from, next) => {
+    const currentRouteGuard = getRouteGuardType(to);
+
+    if (currentRouteGuard === 'auth' && !authState.user) {
+      next(authFailRedirect);
+    } else if (currentRouteGuard === 'no-auth' && authState.user) {
+      next(noAuthFailRedirect);
+    } else {
+      next();
+    }
+  });
 });
